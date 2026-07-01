0e2f37fdef69bbb4153c6bde18b28ab5816a717b
RaRap <rapseedza01@gmail.com>
2026-07-01 10:06:13 +0700

    ...

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -1280,7 +1280,7 @@
                         <svg class="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
                         </svg>
-                        เพิ่มไฟล์ (<span x-text="5 - files.length"></span> ไฟล์ที่เหลือ)
+                        เพิ่มไฟล์ (<span x-text="5 - files.length"></span> ไฟล์ที่เหลือ )
                     </button>
                 </div>
             </div>
